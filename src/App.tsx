0710ed87f02e910e135cee01b4fdb7c6a4ecb039
Randy-diebold <randy.diebold@gmail.com>
2021-10-26 17:55:49 -0700

    init dev commit

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Team Charizard</p>
+        <p>Hello Team Charizard dev</p>
         <a
           className="App-link"
           href="https://reactjs.org"
