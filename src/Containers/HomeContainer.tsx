72bfb758f0c4bdd3d583aa31aeec560e27660432
Charlie Malave <malavecharles@gmail.com>
2021-10-28 13:44:58 -0400

    pushing changes prior to transfering to fresh react app

---
+++ src/Containers/HomeContainer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomeContainer = () => {
+
+    return (
+        <div>
+            this is the home container
+        </div>
+    )
+};
+
+export default HomeContainer;
